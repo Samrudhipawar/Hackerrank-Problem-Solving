b75b90bfe22fc88a97af81ebd8d9ae18cae26520
Samrudhipawar <163659791+Samrudhipawar@users.noreply.github.com>
2024-07-13 20:08:56 +0530

    Update cavity-map.py

--- Algorithms/Implementation/cavity-map.py
+++ Algorithms/Implementation/cavity-map.py
@@ -1,18 +1,12 @@
-# Cavity Map
-# Developer: Murillo Grubler
-# Link: https://www.hackerrank.com/challenges/cavity-map/problem
-# Time Complexity = O(nˆ2)
-
 def cavityMap(grid):
     total = len(grid)
     for i in range(1, total - 1):
         for j in range(1, total - 1):
-            if grid[i][j] > max(grid[i][j-1],grid[i][j+1],grid[i-1][j],grid[i+1][j]):
+            if grid[i][j] > max(grid[i][j-1], grid[i][j+1], grid[i-1][j], grid[i+1][j]):
                 grid[i][j] = 'X'
 
     for i in grid:
-        print ("".join(i))
-        print ("\n")
+        print("".join(i))
 
 if __name__ == '__main__':
     n = int(input())
